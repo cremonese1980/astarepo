f295a9f14f6092597b046ce47e9a70fb3f704d8f
gcrem <cremonese1980@gmail.com>
2015-05-19 20:14:50 +0200

    Quarto

--- asta-web2/asta-web2/src/main/java/it/astaweb/service/StudentService.java
+++ asta-web2/asta-web2/src/main/java/it/astaweb/service/StudentService.java
@@ -2,6 +2,7 @@ package it.astaweb.service;
 
 import it.astaweb.model.Student;
 
+
 public interface StudentService {
 	  Student save(Student student);
 	  boolean findByLogin(String userName, String password);

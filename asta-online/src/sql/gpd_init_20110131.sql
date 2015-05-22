5aa2ad68393d484bc60073c4eb056278afe1a9e3
gcrem <cremonese1980@gmail.com>
2015-05-22 13:52:43 +0200

    deleted old project

--- asta-online/src/sql/gpd_init_20110131.sql
+++
@@ -1,2 +0,0 @@
-
-insert  into `authorities`(`username`,`authority`) values ('admin','ROLE_AMMINISTRATORE'),('advalso','ROLE_BACKOFFICE'),('amministratore','ROLE_AMMINISTRATORE'),('controlroom','ROLE_CONTROLROOM'),('mauri','ROLE_USER'),('olivetti','ROLE_BACKOFFICE'),('telecomitalia','ROLE_BACKOFFICE');
